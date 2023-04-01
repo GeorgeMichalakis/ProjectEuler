9bbce8c1665cc0a27267deb9c1dd422939322705
GeorgeMichalakis <gmichalakis26@hotmail.com>
2023-04-01 17:56:37 +0200

    Init commit - Problem1

---
+++ Problem1.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+
+import sys
+
+def sumOf(n):
+    sum_of_threes = 3 * (n // 3) * ((n // 3) + 1) // 2
+    sum_of_fives = 5 * (n // 5) * ((n // 5) + 1) // 2
+    sum_of_fifteens = 15 * (n // 15) * ((n // 15) + 1) // 2
+    return sum_of_threes + sum_of_fives - sum_of_fifteens
+
+t = int(input().strip())
+for a0 in range(t):
+    n = int(input().strip())
+    print (sumOf(n-1))
